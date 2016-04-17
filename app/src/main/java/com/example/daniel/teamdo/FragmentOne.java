2d9dda75a6e8d2505e032344de3bec35a187193d
Daniel <abcd>
2016-04-17 04:18:15 +0200

    4 neue Classes und 3 neue xml files fürs swipe geadded #yolo #bestteam #apppros

---
+++ app/src/main/java/com/example/daniel/teamdo/FragmentOne.java
@@ -0,0 +1,19 @@
+package com.example.daniel.teamdo;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Daniel on 17.04.2016.
+ */
+public class FragmentOne extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_one_layout, container, false);
+    }
+}
